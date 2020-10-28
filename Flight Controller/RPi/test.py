560f8a3b3012f41ccb03dd6c1aaa53596b25734d
Louie Henson <louie.henson@yahoo.com>
2020-10-28 08:07:44 +0800

    update python test

--- Flight Controller/RPi/test.py
+++ Flight Controller/RPi/test.py
@@ -1,10 +1,10 @@
-import RPi.GPIO as IO #calling header file which helps us use GPIO’s of PI
+import RPi.GPIO as IO #calling header file which helps us use GPIOs of PI
 
 import time #calling time to provide delays in program
 
 IO.setwarnings(False) #do not show any warnings
 
-IO.setmode (IO.BCM) #we are programming the GPIO by BCM pin numbers. (PIN35 as ‘GPIO19’)
+IO.setmode (IO.BCM) #we are programming the GPIO by BCM pin numbers. (PIN35 as GPIO19)
 
 IO.setup(19,IO.OUT) # initialize GPIO19 as an output.
 
